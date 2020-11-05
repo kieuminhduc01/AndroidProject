0bae4a7d5eccfe1165c76508436fda2c3a78458c
kieuminhduc01 <kieuminhduc01@gmail.com>
2020-11-05 15:09:40 +0700

    .

--- app/src/main/java/com/example/appexcerse/MainActivity.java
+++ app/src/main/java/com/example/appexcerse/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.appexcerse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.appexcerse.area.Client.chucNang.FragmentClientExercise;
+import com.example.appexcerse.area.Client.chucNang.FragmentClientPersonalInformation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemReselectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentClientExercise()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemReselectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_exercise:
+                            selectedFragment = new FragmentClientExercise();
+                            break;
+
+                        case R.id.nav_personal_information:
+                            selectedFragment = new FragmentClientPersonalInformation();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
+
 }
--- app/src/main/java/com/example/appexcerse/area/Client/chucNang/FragmentClientExercise.java
+++ app/src/main/java/com/example/appexcerse/area/Client/chucNang/FragmentClientExercise.java
@@ -1,12 +1,13 @@
-package com.example.navigationbar;
+package com.example.appexcerse.area.Client.chucNang;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.appexcerse.R;
 
 /**
  * A simple {@link Fragment} subclass.
