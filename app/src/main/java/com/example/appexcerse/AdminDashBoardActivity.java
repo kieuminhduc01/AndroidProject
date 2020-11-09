7a98d855e9915556e95731a5536519267035ea63
DATNGUYEN\DAT NGUYEN <nguyendat.yl.99@gmail.com>
2020-11-09 09:41:13 +0700

    Thêm dependencies và tạo activity_dash_board cho admin .

---
+++ app/src/main/java/com/example/appexcerse/AdminDashBoardActivity.java
@@ -0,0 +1,15 @@
+package com.example.appexcerse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminDashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
+
+}
